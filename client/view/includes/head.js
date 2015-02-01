83701485f12c2fbb37d1909df13164cd5e98b647
systems07 <Sharath@Sharaths-MacBook-Pro.local>
2015-02-01 18:07:29 -0500

    file locations update

--- client/view/includes/head.js
+++ client/view/includes/head.js
@@ -1,8 +1,8 @@
 document.write("    <meta charset=\"utf-8\">");
 document.write("    <meta name=\"viewport\" content=\"initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width\">");
-document.write("    <link href=\"client\/view\/css\/mtree.css\" rel=\"stylesheet\">");
-document.write("    <link href=\"..\/css\/bootstrap.css\" rel=\"stylesheet\">");
+document.write("    <link href=\"css\/mtree.css\" rel=\"stylesheet\">");
+document.write("    <link href=\"css\/bootstrap.css\" rel=\"stylesheet\">");
 document.write("    <link rel=\"stylesheet\" href=\"http:\/\/maxcdn.bootstrapcdn.com\/font-awesome\/4.3.0\/css\/font-awesome.min.css\">");
-document.write("    <link href=\"..\/lib\/xcharts.css\" rel=\"stylesheet\">");
-document.write("    <link href=\"..\/css\/custom.css\" rel=\"stylesheet\">");
+document.write("    <link href=\"lib\/xcharts.css\" rel=\"stylesheet\">");
+document.write("    <link href=\"css\/custom.css\" rel=\"stylesheet\">");
 document.write("    <script src=\"https:\/\/cdnjs.cloudflare.com\/ajax\/libs\/angular.js\/1.3.9\/angular.js\"><\/script>");
