f5bfa220291d07e7a76fa84bbdf49f996aaa9a0a
systems07 <Sharath@Sharaths-MacBook-Pro.local>
2015-02-02 21:27:50 -0500

    responsive table

--- client/js/clientController.js
+++ client/js/clientController.js
@@ -1,8 +1,17 @@
 var countryApp = angular.module('countryApp', []);
       countryApp.controller('CountryCtrl', function ($scope, $http){
-        $http.get('../../data/countries.json').success(function(data) {
+        $http.get('../../data/utilization.json').success(function(data) {
           $scope.countries = data;
         });
         $scope.sortField = 'population';
-        $scope.reverse = true;
+        $scope.reverse = false;
+		
+        $scope.setSelected = function() {
+       	if ($scope.lastSelected) {
+         $scope.lastSelected.selected = '';
+       	}
+       	this.selected = 'selected';    	
+       	$scope.lastSelected = this;
+       	} 	   
+
       });
